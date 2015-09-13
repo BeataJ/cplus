f14258efaa67bc5bccb138a17ee883010ee29c55
Bob JJ <bobjas777@yahoo.ca>
2015-09-13 15:37:37 -0700

    added size parameter  to sumOfElements  function and  now it works

--- array_fun_args.cpp
+++ array_fun_args.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 using namespace std;
-int sumOfElements(int X[]);
+int sumOfElements(int X[],int size);
 
 int main() {
 	int A[]={1,2,3,4,5};
-	cout <<"Size of A :" << sizeof(A)<<"Size of each element"<<sizeof(A[0])<<"Lenght of array( number of elements ="<<sizeof(A)/sizeof(A[0])<<endl;
-	int total=sumOfElements(A);
+	cout <<"Size of A :" << sizeof(A)<<" Size of each element "<<sizeof(A[0])<<" Lenght of array( number of elements = "<<sizeof(A)/sizeof(A[0])<<endl;
+	int length=sizeof(A)/sizeof(A[0]);
+	int total=sumOfElements(A,length);
 	cout<<"Sum of elements = "<<total<<endl;
 }
  
-int sumOfElements(int X[]) {
+int sumOfElements(int X[],int size) {
 	int sum=0;
-	int size=sizeof(X)/sizeof(X[0]);
+	
 	//
 	for (int i=0;i<size;i++) sum+=X[i];
 	return sum;
