68b5df308d199f254ed3f295908b10397f578ebb
Bobj <bobjas777@yahoo.ca>
2015-09-13 23:38:49 -0700

    for now commit  changes  with errors

--- rk_ref.cpp
+++ rk_ref.cpp
@@ -2,14 +2,14 @@
 #include<iostream>
 using namespace std;
 //prototype  declarations 
-void wspolczynniki(double *a,double *b, double *c);
-double delta(double a,doubleb,double c);//
+void wspolczynniki(double*  a,double* b, double* c);
+double delta(double a,double b,double c);//
 //double piewiastki(double a, double b, double c);
 
 //main function
 int main() {
-wspolczynniki(&a,&b,&c);
-d=delta(a,b,c);
+wspolczynniki(&a ,&b, &c);
+double d=delta(a,b,c);
 if (d<0) cout<<" Rownanie kwadratowe nie posiada pierwiastkow  w zbiorze liczb rzeczywistych"<<endl;
 else if (d==0) cout <<"Pojedynczy pierwiastek rownania "<<endl;
 else if (d>0) cout <<" Dwa pierwiastki rownania : "<<endl;
@@ -17,12 +17,12 @@ else if (d>0) cout <<" Dwa pierwiastki rownania : "<<endl;
 void wspolczynniki(double *a,double *b, double*c) {
   cout <<" Wartosc wspolczynnika a ";
   cin>> *a;
-  cout " Wartosc wspolczynnika b ";
+  cout<<" Wartosc wspolczynnika b ";
   cin>>*b;
-  cout<<" Wartosc wspolczynnika c ";
+  cout << " Wartosc wspolczynnika c ";
   cin>> *c;
 }
 double delta(double a,double b,double c)
 {
-return b*b -4*a*c
+  return b*b -4*a*c;
 }
